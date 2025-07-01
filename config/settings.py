08b4562956c79ce977b533305ecf3fb764ddf671
JinLong Wu <2474323762@qq.com>
2025-07-01 17:29:21 +0800

    feat: 增加task应用

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # Allow all hosts for development; change in production
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     # Local apps
     'accounts',
+    'task',
 ]
 
 MIDDLEWARE = [
